dac2f238605dfdef31103e8f0905febdf70f50a9
TAEEEE30 <monzamk@gmail.com>
2025-07-01 17:02:17 +0700

    push project

---
+++ src/components/Textsize.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const TextWelcome = () => {
+  return (
+    <div className='text-[96px]'>
+      <h1 className="text-6xl md:text-8xl font-orbitron font-black uppercase mb-4">
+            <span className="neon-text animate-glow">WELCOME</span>
+            <br />
+            {/* <span className="text-neon-blue animate-glow delay-500">TO THE</span>
+            <br />
+            <span className="text-neon-purple animate-glow delay-1000">MATRIX</span> */}
+          </h1>
+
+    </div>
+  )
+}
